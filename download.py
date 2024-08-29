16bec5b5e43a59e9d81df4db6b99e3d68d539171
guoql666 <30526996+guoql666@users.noreply.github.com>
2024-08-29 15:53:21 +0800

    Update download.py

--- download.py
+++ download.py
@@ -5,6 +5,7 @@ from common import *
 def getPicByMD5(md5 : str, folder : Union[str, None]) -> Union[bool, bytes]:
     if folder is None:
         folder = 'base'
+    md5 = md5.lower()
     try:
         with open(upload_url + f"{md5}-{folder}",'rb') as file:
             data = file.read()
