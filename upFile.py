dd012b96b48cd9f0fda8726e5dda3bb755cae095
guoql666 <30526996+guoql666@users.noreply.github.com>
2024-09-01 09:03:04 +0800

    Update upFile.py

--- upFile.py
+++ upFile.py
@@ -4,7 +4,7 @@ import hashlib
 from time import time
 import aiohttp
 
-async def UploadFile(payload,folder : Union[str, None]) -> bool:
+async def UploadFile(payload,folder : Union[str, None], token : str) -> bool:
     if folder is None:
         folder = "base"
     # 防止异步造成文件名资源互斥，用时间来区分
@@ -13,7 +13,7 @@ async def UploadFile(payload,folder : Union[str, None]) -> bool:
     md5_hash = hashlib.md5()
     # 计算size
     size = 0
-    with open(upload_url + f"{temp_filename}-{folder}","wb") as file:
+    with open(upload_url + f"/{token}/" + f"{temp_filename}-{folder}","wb") as file:
         while True:
             chunk = await payload.read_chunk()
             if not chunk:
@@ -27,15 +27,15 @@ async def UploadFile(payload,folder : Union[str, None]) -> bool:
     # 计算md5文件名
     filename = md5_hash.hexdigest()
     # 重命名
-    os.rename(upload_url + f"{temp_filename}-{folder}", upload_url + f"{filename}-{folder}")
+    os.rename(upload_url + f"/{token}/" + f"{temp_filename}-{folder}", upload_url + f"/{token}/" + f"{filename}-{folder}")
     #若size为0，说明文件未正常传输或为空文件
     if size == 0:
-        os.remove(upload_url + f"{filename}-{folder}")
+        os.remove(upload_url + f"/{token}/" + f"{filename}-{folder}")
         return False,None
     return True,filename
 
 
-async def UploadFileFromUrl(url: str, folder: Union[str, None]) -> Tuple[bool, Union[None, str]]:
+async def UploadFileFromUrl(url: str, folder: Union[str, None], token: str) -> Tuple[bool, Union[None, str]]:
     if folder is None:
         folder = "base"
     async with aiohttp.request("GET",url) as res:
@@ -47,6 +47,6 @@ async def UploadFileFromUrl(url: str, folder: Union[str, None]) -> Tuple[bool, U
     if len(data) == 0:
         return False,None
     filename = hashlib.md5(data).hexdigest()
-    with open(upload_url + f"{filename}-{folder}","wb") as file:
+    with open(upload_url + f"/{token}/" + f"{filename}-{folder}","wb") as file:
         file.write(data)
     return True,filename
