6281c481c267c4c46d8b9b37a6ba2a294f96bc27
guoql666 <30526996+guoql666@users.noreply.github.com>
2024-09-01 09:02:22 +0800

    Update download.py

--- download.py
+++ download.py
@@ -2,15 +2,27 @@ from typing import Union
 from common import *
 
 
-def getPicByMD5(md5 : str, folder : Union[str, None]) -> Union[bool, bytes]:
+def getPicByMD5(md5 : str, folder : Union[str, None],token: str) -> Union[bool, bytes]:
     if folder is None:
         folder = 'base'
     md5 = md5.lower()
     try:
-        with open(upload_url + f"{md5}-{folder}",'rb') as file:
+        with open(upload_url + f"/{token}/" + f"{md5}-{folder}",'rb') as file:
             data = file.read()
         return data
     except FileNotFoundError as e:
         print(e)
         return False
-    
+
+
+def getListPic(folder: Union[str, None], token: str) -> dict:
+    pathList = os.listdir(upload_url + f"/{token}/" )
+    result_dict = dict()
+    for path in pathList:
+        md5,raw_folder = path.split("-")
+        if folder is None or raw_folder == folder:
+            try:
+                result_dict[raw_folder].append(md5)
+            except:
+                result_dict[raw_folder] = [md5]
+    return result_dict
