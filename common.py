7e4d45f661f1af657a8eba1620e62a71909e6c00
guoql666 <30526996+guoql666@users.noreply.github.com>
2024-09-01 09:02:02 +0800

    Update common.py

--- common.py
+++ common.py
@@ -14,6 +14,23 @@ def check_md5(md5:str) -> bool:
         return False
     
 
-def md5_calc(md5_hash,data):
+def md5_calc(md5_hash, data: bytes):
     md5_hash.update(data)
     return md5_hash
+
+
+def folder_init():
+    for token in token_list:
+        if not os.path.isdir(upload_url + f"/{token}/"):
+            os.mkdir(upload_url + f"/{token}/")
+
+
+def IsPicExist(target_md5: str, folder: str, token: str) -> bool:
+    if folder is None:
+        folder = "base"
+    pathList = os.listdir(upload_url + f"/{token}/" )
+    for path in pathList:
+        md5,raw_folder = path.split("-")
+        if folder == raw_folder and md5 == target_md5:
+            return True
+    return False
